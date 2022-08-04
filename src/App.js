66efcfae1b0f0c0ccbe9ea0dbb681882916f59c3
alenBcommer <alen@commer.com>
2022-08-04 12:11:06 +0200

    Multiple inputs and controlled input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 /* import Final from './tutorial/11-react-router/final/index' */
-import Setup from './tutorial/4-forms/setup/1-controlled-inputs';
+import Setup from './tutorial/4-forms/setup/2-multiple-inputs';
 function App() {
   return (
     <div className='container'>
--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,10 +8,20 @@ import React, { useState } from 'react';
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState('');
   const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(firstName, email);
+    if (firstName && email) {
+      const person = { id: new Date().getTime().toString(), firstName, email }
+
+      setPeople((current) => {
+        return [...people, person]
+      });
+      setFirstName('');
+      setEmail('');
+    }
+
   }
   return <>
     <article>
@@ -26,6 +36,15 @@ const ControlledInputs = () => {
         </div>
         <button type='submit'>Add Person</button>
       </form>
+      {people.map((person) => {
+        const { id, firstName, email } = person;
+        return (
+          <div key={id} className='item'>
+            <h4>{firstName}</h4>
+            <p>{email}</p>
+          </div>
+        )
+      })}
     </article>
 
   </>;

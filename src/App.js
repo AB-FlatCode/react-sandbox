34c30ba336322fabcad7ef1a9fdd879b38130bde
Alen <info@flatcode.hr>
2022-08-14 19:47:35 +0200

    UseContext setup completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 /* import Final from './tutorial/11-react-router/final/index' */
-import Setup from "./tutorial/6-useReducer/setup";
+import Setup from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
 function App() {
   return (
     <div className="container">
